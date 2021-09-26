e4adf84309b8faf4c0605b4fd976c4ba7fe0be18
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-09-26 20:58:00 +0530

    day_12

--- Day_11/middleWare/registerChecks.js
+++ Day_11/middleWare/registerChecks.js
@@ -1,3 +1,28 @@
-const registerChecks=(req, res, next) =>{
+const {emailValidate, passwordValidate}=require('../utlis/Validation')
 
+const registerInitialChecks=(req, res, next) =>{
+    const {email , password , confirmPassword}=req.body;
+
+    if(
+        typeof email === 'string' &&
+        typeof password === 'string' &&
+        typeof confirmPassword === 'string' &&
+        email.length > 0 && 
+        password.length > 0 && 
+        confirmPassword === password &&
+        emailValidate(email) &&
+        passwordValidate(password)
+    ){
+        next();
+        
+    }else {
+        return res.status(400).json({
+            error: 'email, password and confirmPassword must be string'
+        })
+        
+    }
+    
+    
 }
+
+module.exports =registerInitialChecks
